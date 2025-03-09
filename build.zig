50cc133bde870231c02098d547b80bd6424d94e9
RecursiveError <guilhermesschultz.contato@gmail.com>
2025-03-08 22:18:01 -0300

    add lib

--- build.zig
+++ build.zig
@@ -3,9 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    _ = b.addModule("xml-parser", .{
+
+    const tracer = b.dependency("zig_tracer", .{}).module("zig-tracer");
+
+    const parser = b.addModule("xml-parser", .{
         .root_source_file = b.path("src/mod.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    parser.addImport("tracer", tracer);
 }
